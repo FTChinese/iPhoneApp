d82a06fb2b219d2a4912ba8f376a6e92be506310
Oliver Zhang <oli.zhang@gmail.com>
2014-12-25 09:17:02 +0800

    Initial Commit

---
+++ FT Academy/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FT Academy
+//
+//  Created by Zhang Oliver on 14/12/25.
+//  Copyright (c) 2014年 Zhang Oliver. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ FT AcademyTests/FT_AcademyTests.swift
@@ -0,0 +1,36 @@
+//
+//  FT_AcademyTests.swift
+//  FT AcademyTests
+//
+//  Created by Zhang Oliver on 14/12/25.
+//  Copyright (c) 2014年 Zhang Oliver. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class FT_AcademyTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
