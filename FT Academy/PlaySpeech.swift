4bf706499f5b37571d878ebfe117b98a7816e678
Oliver Zhang <oli.zhang@gmail.com>
2017-03-26 16:58:21 +0800

    audio speech scene

---
+++ FT Academy/PlaySpeech.swift
@@ -0,0 +1,27 @@
+//
+//  PlaySpeech.swift
+//  FT中文网
+//
+//  Created by ZhangOliver on 2017/3/26.
+//  Copyright © 2017年 Financial Times Ltd. All rights reserved.
+//
+
+import UIKit
+import AVKit
+import AVFoundation
+
+import MediaPlayer
+
+
+class PlaySpeech: UIViewController {
+    deinit {
+        print ("audio view closed")
+    }
+    override func loadView() {
+        super.loadView()
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
