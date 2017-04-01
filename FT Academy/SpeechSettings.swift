689bb296beaa47a26c4c777b6a10d88cdaadf468
Oliver Zhang <oli.zhang@gmail.com>
2017-04-01 21:33:25 +0800

    accent picker

--- FT Academy/SpeechSettings.swift
+++ FT Academy/SpeechSettings.swift
@@ -13,7 +13,20 @@ class SpeechSettings: UIViewController, UIPickerViewDataSource, UIPickerViewDele
     @IBAction func closeSetting(_ sender: UIButton) {
         self.dismiss(animated: true, completion: nil)
     }
-    let pickerData = ["英国","美国","澳大利亚","印度","新加坡","加拿大"]
+    let englishAccent = [
+        "英国":"en-GB",
+        "美国":"en-US",
+        "澳大利亚":"en-AU",
+        "南非":"en-ZA",
+        "爱尔兰":"en-IE"
+    ]
+    var englishAccentData = [String]()
+    let chineseAccent = [
+        "中国大陆":"zh-CN",
+        "香港":"zh-HK",
+        "台湾":"zh-TW"
+    ]
+    var chineseAccentData = [String]()
     
     override func loadView() {
         super.loadView()
@@ -22,30 +35,56 @@ class SpeechSettings: UIViewController, UIPickerViewDataSource, UIPickerViewDele
         } else {
             print ("this is not a popover")
         }
-        
+        englishAccentData = Array(englishAccent.keys)
         myPicker.dataSource = self
         myPicker.delegate = self
+        chineseAccentData = Array(chineseAccent.keys)
+        chineseAccentPick.dataSource = self
+        chineseAccentPick.delegate = self
         //self.popoverPresentationController?.delegate = self
     }
     
     @IBOutlet weak var myPicker: UIPickerView!
 
     
+    @IBOutlet weak var chineseAccentPick: UIPickerView!
     
     func numberOfComponents(in pickerView: UIPickerView) -> Int {
         return 1
     }
     
-    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-        return pickerData.count
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {        
+        if pickerView.restorationIdentifier == "English Accent Picker" {
+        return englishAccentData.count
+        } else {
+            return chineseAccentData.count
+        }
     }
     
     func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
-        return pickerData[row]
+        if pickerView.restorationIdentifier == "English Accent Picker" {
+        return englishAccentData[row]
+        } else {
+            return chineseAccentData[row]
+        }
     }
     
     func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
-        print (pickerData[row])
+        var language = "en"
+        var accent = "en-GB"
+        if pickerView.restorationIdentifier == "English Accent Picker" {
+            language = "en"
+            accent = englishAccent[englishAccentData[row]] ?? "en-GB"
+        } else {
+            language = "ch"
+            accent = chineseAccent[chineseAccentData[row]] ?? "zh-CN"
+        }
+        changeAccent(language, accent: accent)
+    }
+    
+    func changeAccent (_ language: String, accent: String) {
+        // TODO: - Update Parent View's Accent and Save
+        print("change \(language) into \(accent)")
     }
     
 
